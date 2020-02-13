6e55688f483635999ad29c4512eae02fc9800357
Ruoyao Wang <ruoyaow@gl-login1.arc-ts.umich.edu>
2020-02-13 11:11:11 -0500

    submit script

--- submit_job.sh
+++ submit_job.sh
@@ -3,7 +3,6 @@
 #“#SBATCH” directives that convey submission options:
 
 #SBATCH --job-name=baseline_test
-#SBATCH --mail-user=ruoyaow@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
@@ -12,13 +11,13 @@
 #SBATCH --time=3:00:00
 #SBATCH --account=mihalcea1
 #SBATCH --partition=standard
-#SBATCH --output=/home/%u/LifeQA-methodology/logs/%x-%j.log
-#SBATCH --workdir=/home/ruoyaow/LifeQA-methodology
 # The application(s) to execute along with its input arguments and options:
 echo Started!
-source /home/ruoyaow/.bashrc
+module load python3.6-anaconda
+echo hook:
+eval "$(conda shell.bash hook)"
 echo Sourcing
-source activate NLPenv0
+conda activate NLPenv0
 echo Sourced
 python train.py
 echo done
