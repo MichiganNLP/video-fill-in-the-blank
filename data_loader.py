3473fa749416f7524ce3d6d6f4f8a0d1731d1807
Ruoyao Wang <wsxzwps43@126.com>
2020-02-15 12:52:37 -0500

    add data loader

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+import torch
 from nltk.corpus import wordnet
 import json
 import math
@@ -7,7 +8,7 @@ import nltk
 
 class ActivityNetCaptionDataset(Dataset):
 
-    def __init__(self, data_file, word_dict):
+    def __init__(self, textFile, word_dict, isTrain=True):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -15,10 +16,24 @@ class ActivityNetCaptionDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.data = self.parseData(data_file)
+        self.textFile = textFile
         self.word_dict = word_dict
+        self.answerWordDict = {}
+        self.isTrain = isTrain
+        self.THRESHOLD = 500
+
+        textFeature = self.getTextFeatures(textFile, isTrain)
+        data = self.getFeatures(textFeature)
+
+    def getVideoFeatures(self, key, startFrame, endFrame):
+        feature_h5 = videoFeature[key]['c3d_features']
+        shape = feature_h5.shape
+        feature_np = np.zeros(shape)
+        feature_h5.read_direct(feature_np)
+        feature = feature_np.mean(axis=0)
+        return torch.FloatTensor(feature).view(1, -1)
     
-    def gen(self, text, parsed_sentence):
+    def gen(self, text, parsed_sentence, isTrain):
         position = []
         for i in range(len(parsed_sentence)):
             if parsed_sentence[i][1] == 'JJ' or parsed_sentence[i][1] == 'NN':
@@ -26,16 +41,25 @@ class ActivityNetCaptionDataset(Dataset):
 
         while len(position): 
             idx = random.sample(position, 1)[0]
-            if text[idx] in word_dict and word_dict[text[idx]] >= THRESHOLD:
+            if text[idx] in self.answerWordDict and self.answerWordDict[text[idx]] >= self.THRESHOLD and isTrain:
                 position.remove(idx)
                 continue
             new_sentence = text[:]
+
             correct_word = new_sentence[idx]
+            if isTrain:
+                if correct_word in self.answerWordDict:
+                    self.answerWordDict[correct_word] += 1
+                else:
+                    self.answerWordDict[correct_word] = 1
+
             new_sentence[idx] = '[MASK]'
             return new_sentence, correct_word
+        
+        return ()
 
-    def parseData(self, data_file):
-        with open(data_file, 'r') as f:
+    def getTextFeatures(self, textFile, isTrain=True):
+        with open(textFile, 'r') as f:
             raw = json.load(f)
         
         data = []
@@ -47,9 +71,37 @@ class ActivityNetCaptionDataset(Dataset):
                 sentence = raw[key]['sentences'][i]
                 text = nltk.word_tokenize(sentence.strip().lower())
                 parsed_sentence = nltk.pos_tag(text)
-                masked_sentence, label = self.gen(text, parsed_sentence)
-                data.append((key, start_frame, end_frame, masked_sentence, label))
+                out = self.gen(text, parsed_sentence, isTrain)
+                if len(out)==2:
+                    masked_sentence, label = out
+                    data.append([key, start_frame, end_frame, masked_sentence, label])
+        
+        sorted_words = sorted(wordDict.items(), key= lambda k : k[1]["freq"], reverse=True)
+
+        for i in range(len(data)):
+            featureDim = 1000
+            textFeature = torch.zeros(1, featureDim)
+            for j in range(featureDim):
+                for word in data[i][3]:
+                    if word == sorted_words[j][0]:
+                        textFeature[0, j] += 1
+                # if sorted_words[j][0] in data[i][3]:
+                #     textFeature[0,j] = 1
+            data[i].append(textFeature)
+        
         return data
+
+    def getFeatures(self, textData):
+        features = []
+        for data in textData:
+            videoFeature = getVideoFeatures(data[0], data[1], data[2])
+            if data[4] in wordDict:
+                label = torch.tensor([wordDict[data[4]]["id"]])
+            else:
+                print(data[4])
+                label = torch.tensor([21086]) #should not happen just in case
+            features.append([data[5], videoFeature, label])
+        return features
 
     def __len__(self):
         return len(self.data)
--- train.py
+++ train.py
@@ -26,87 +26,87 @@ valTextFile = f"{folder}/val_1.json"
 with open('word_dict.pkl', 'rb') as f:
     wordDict = pickle.load(f)
 
-answerWordDict = {}
-THRESHOLD = 500
+# answerWordDict = {}
+# THRESHOLD = 500
 
-def getVideoFeatures(key, startFrame, endFrame):
-    feature_h5 = videoFeature[key]['c3d_features']
-    shape = feature_h5.shape
-    feature_np = np.zeros(shape)
-    feature_h5.read_direct(feature_np)
-    feature = feature_np.mean(axis=0)
-    return torch.FloatTensor(feature).view(1, -1)
+# def getVideoFeatures(key, startFrame, endFrame):
+#     feature_h5 = videoFeature[key]['c3d_features']
+#     shape = feature_h5.shape
+#     feature_np = np.zeros(shape)
+#     feature_h5.read_direct(feature_np)
+#     feature = feature_np.mean(axis=0)
+#     return torch.FloatTensor(feature).view(1, -1)
 
-def gen(text, parsed_sentence, isTrain):
-    position = []
-    for i in range(len(parsed_sentence)):
-        if parsed_sentence[i][1] == 'JJ' or parsed_sentence[i][1] == 'NN':
-            position.append(i)
+# def gen(text, parsed_sentence, isTrain):
+#     position = []
+#     for i in range(len(parsed_sentence)):
+#         if parsed_sentence[i][1] == 'JJ' or parsed_sentence[i][1] == 'NN':
+#             position.append(i)
 
-    while len(position): 
-        idx = random.sample(position, 1)[0]
-        if text[idx] in answerWordDict and answerWordDict[text[idx]] >= THRESHOLD and isTrain:
-            position.remove(idx)
-            continue
-        new_sentence = text[:]
+#     while len(position): 
+#         idx = random.sample(position, 1)[0]
+#         if text[idx] in answerWordDict and answerWordDict[text[idx]] >= THRESHOLD and isTrain:
+#             position.remove(idx)
+#             continue
+#         new_sentence = text[:]
 
-        correct_word = new_sentence[idx]
-        if isTrain:
-            if correct_word in answerWordDict:
-                answerWordDict[correct_word] += 1
-            else:
-                answerWordDict[correct_word] = 1
+#         correct_word = new_sentence[idx]
+#         if isTrain:
+#             if correct_word in answerWordDict:
+#                 answerWordDict[correct_word] += 1
+#             else:
+#                 answerWordDict[correct_word] = 1
 
-        new_sentence[idx] = '[MASK]'
-        return new_sentence, correct_word
+#         new_sentence[idx] = '[MASK]'
+#         return new_sentence, correct_word
     
-    return ()
+#     return ()
 
-def getTextFeatures(textFile, isTrain=True):
-    with open(textFile, 'r') as f:
-        raw = json.load(f)
+# def getTextFeatures(textFile, isTrain=True):
+#     with open(textFile, 'r') as f:
+#         raw = json.load(f)
     
-    data = []
+#     data = []
 
-    for key in raw.keys():
-        total_events = len(raw[key]['sentences'])
-        for i in range(total_events):
-            start_frame = math.floor(raw[key]['timestamps'][i][0] * 2)
-            end_frame = math.ceil(raw[key]['timestamps'][i][1] * 2)
-            sentence = raw[key]['sentences'][i]
-            text = nltk.word_tokenize(sentence.strip().lower())
-            parsed_sentence = nltk.pos_tag(text)
-            out = gen(text, parsed_sentence, isTrain)
-            if len(out)==2:
-                masked_sentence, label = out
-                data.append([key, start_frame, end_frame, masked_sentence, label])
+#     for key in raw.keys():
+#         total_events = len(raw[key]['sentences'])
+#         for i in range(total_events):
+#             start_frame = math.floor(raw[key]['timestamps'][i][0] * 2)
+#             end_frame = math.ceil(raw[key]['timestamps'][i][1] * 2)
+#             sentence = raw[key]['sentences'][i]
+#             text = nltk.word_tokenize(sentence.strip().lower())
+#             parsed_sentence = nltk.pos_tag(text)
+#             out = gen(text, parsed_sentence, isTrain)
+#             if len(out)==2:
+#                 masked_sentence, label = out
+#                 data.append([key, start_frame, end_frame, masked_sentence, label])
     
-    sorted_words = sorted(wordDict.items(), key= lambda k : k[1]["freq"], reverse=True)
+#     sorted_words = sorted(wordDict.items(), key= lambda k : k[1]["freq"], reverse=True)
 
-    for i in range(len(data)):
-        featureDim = 1000
-        textFeature = torch.zeros(1, featureDim)
-        for j in range(featureDim):
-            for word in data[i][3]:
-                if word == sorted_words[j][0]:
-                    textFeature[0, j] += 1
-            # if sorted_words[j][0] in data[i][3]:
-            #     textFeature[0,j] = 1
-        data[i].append(textFeature)
+#     for i in range(len(data)):
+#         featureDim = 1000
+#         textFeature = torch.zeros(1, featureDim)
+#         for j in range(featureDim):
+#             for word in data[i][3]:
+#                 if word == sorted_words[j][0]:
+#                     textFeature[0, j] += 1
+#             # if sorted_words[j][0] in data[i][3]:
+#             #     textFeature[0,j] = 1
+#         data[i].append(textFeature)
     
-    return data
+#     return data
 
-def getFeatures(textData):
-    features = []
-    for data in textData:
-        videoFeature = getVideoFeatures(data[0], data[1], data[2])
-        if data[4] in wordDict:
-            label = torch.tensor([wordDict[data[4]]["id"]])
-        else:
-            print(data[4])
-            label = torch.tensor([21086]) #should not happen just in case
-        features.append([data[5], videoFeature, label])
-    return features
+# def getFeatures(textData):
+#     features = []
+#     for data in textData:
+#         videoFeature = getVideoFeatures(data[0], data[1], data[2])
+#         if data[4] in wordDict:
+#             label = torch.tensor([wordDict[data[4]]["id"]])
+#         else:
+#             print(data[4])
+#             label = torch.tensor([21086]) #should not happen just in case
+#         features.append([data[5], videoFeature, label])
+#     return features
 
 
 def train(data, max_epoch, model, optimizer, criterion):
@@ -145,12 +145,12 @@ def evaluation(test_data, model):
 
 print("start")
 
-trainText = getTextFeatures(trainTextFile)
-trainFeatures = getFeatures(trainText)
+trainDataset = ActivityNetCaptionDataset(trainTextFile, word_dict)
+trainLoader = DataLoader(trainDataset, batch_size=16, shuffle=True, num_workers=4)
 print("successfully load train")
 
-valText = getTextFeatures(valTextFile, isTrain=False)
-valFeatures = getFeatures(valText)
+valDataset = ActivityNetCaptionDataset(valTextFile, word_dict, isTrain=False)
+valLoader = DataLoader(valDataset, batch_size=16, shuffle=True, num_workers=4)
 print("successfully load val")
 
 model = baseline_BOW_VF(1000, 500, len(wordDict))
@@ -158,7 +158,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 max_epoch = 3
 
-model = train(trainFeatures, max_epoch, model, optimizer, criterion)
-evaluation(valFeatures, model)
+model = train(trainLoader, max_epoch, model, optimizer, criterion)
+evaluation(valLoader, model)
 
 
