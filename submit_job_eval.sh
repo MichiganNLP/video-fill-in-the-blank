c5c52440cd11115ef71bcf47b952084269946d6a
Ruoyao Wang <ruoyaow@gl-login2.arc-ts.umich.edu>
2020-03-20 00:28:16 -0400

    local

--- submit_job_eval.sh
+++ submit_job_eval.sh
@@ -2,7 +2,7 @@
 
 #“#SBATCH” directives that convey submission options:
 
-#SBATCH --job-name=baseline_test
+#SBATCH --job-name=eval_scheduler
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=4
 #SBATCH --nodes=1
