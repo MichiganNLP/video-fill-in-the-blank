42177e1ce90cc06ab8e8a41a6298f0d68b64b598
Ruoyao Wang <wsxzwps43@126.com>
2020-04-30 17:44:12 -0400

    val

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,6 +13,6 @@ PATH = 'Checkpoint'
 folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions"
 
 videoFeatures = h5py.File(f"{folder}/ActivityNet_Captions_Video_Features/sub_activitynet_v1-3.c3d.hdf5", 'r')
-trainTextFile = f"{folder}/train.json"
+trainTextFile = f"{folder}/val1.json"
 
-ActivityNetCaptionDataset(trainTextFile, videoFeatures)
+ActivityNetCaptionDataset(trainTextFile, videoFeatures, isTrain=False)
