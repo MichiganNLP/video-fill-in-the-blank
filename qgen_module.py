febfc69e2a85faac5f781cd7c4b7d453ff15d62d
Ruoyao Wang <wsxzwps43@126.com>
2020-05-15 00:30:43 -0400

    debug

--- qgen_module.py
+++ qgen_module.py
@@ -301,7 +301,7 @@ class QGenLightningModel(LightningModule):
                         self.tokenizer.convert_tokens_to_ids(labels[i]))
                 else:
                     print(masked_lm_labels[i, mask_positions[i]].shape)
-                    print(torch.LongTensor(self.tokenizer.convert_tokens_to_ids(labels[i][0])).shape)
+                    print(torch.LongTensor(self.tokenizer.convert_tokens_to_ids(labels[i][0])))
                     masked_lm_labels[i, mask_positions[i]] = torch.LongTensor(self.tokenizer.convert_tokens_to_ids(labels[i][0]))
 
             out.append((text_tensor, video_tensor, mask, segments_tensor, labels, mask_positions, masked_lm_labels,
