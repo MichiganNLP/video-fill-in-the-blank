eb8c8d3151e5ddb22bcb9211e81083cde851cc33
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2020-06-14 03:29:53 +0800

    Add set up for running bag of word model.

--- train.py
+++ train.py
@@ -1,61 +1,11 @@
-import pickle
-import os
-import h5py
-import json
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from collections import Counter
 
 from baseline_BOW_VF import baseline_BOW_VF
 from data_loader_multimodal import ActivityNetCaptionsDataset
-
-print("Very begin")
-folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions"
-videoFeatures = h5py.File(f"{folder}/ActivityNet_Captions_Video_Features/sub_activitynet_v1-3.c3d.hdf5", 'r')
-trainTextFile = f"{folder}/train.json"
-valTextFile = f"{folder}/val_1.json"
-
-
-stop_words = set(stopwords.words('english'))
-
-# small samples testing
-# trainTextFile = 'train.json'
-# valTextFile = 'val.json'
-
-
-def fit(train_text_file, num_tokens):
-    word_to_idx = dict()
-    file = open(train_text_file, 'r')
-    data = json.load(file)
-    # collect all documents into all_sentences
-    documents = list()
-    for key in data:
-        documents.extend(data[key]['sentences'])
-    # tokenize documents
-    tokenzied_docs = [word_tokenize(doc.lower()) for doc in documents]
-    # filter out stop words
-    tokens = [w for t_doc in tokenzied_docs for w in t_doc if w.isalpha()]
-    tokens_no_stops = [t for t in tokens if t not in stop_words]
-    # Get top k tokens by frequency
-    top_k_words = Counter(tokens_no_stops).most_common(num_tokens)
-    # create (word, index) dictionary
-    for word, _ in top_k_words:
-        if word not in word_to_idx:
-            word_to_idx[word] = len(word_to_idx)
-    return word_to_idx
-
-
-def transform(sentence, word_to_idx):
-    vector = torch.zeros(len(word_to_idx))
-    tokens = [w for w in word_tokenize(sentence) if w.isalpha()]
-    token_no_stops = [t for t in tokens if t not in stop_words]
-    for word in token_no_stops:
-        if word in word_to_idx:
-            vector[word_to_idx[word]] += 1
-    return vector.view(1, -1)
+from utils import build_representation, fit
+import pickle
 
 
 def train(data, max_epoch, model, optimizer, criterion):
@@ -63,22 +13,23 @@ def train(data, max_epoch, model, optimizer, criterion):
     running_loss = 0
     for epoch in range(max_epoch):
         for n, batch in enumerate(data):
-            optimizer.zero_grad()
-            text_feature, video_feature, label = batch
 
+            optimizer.zero_grad()
+
+            text_feature, video_feature, labels = batch
             # use GPU
             text_feature = text_feature.cuda()
             video_feature = video_feature.cuda()
-            label = label.cuda()
+            labels = labels.squeeze(dim=1)
+            labels = labels.cuda()
 
             output = model(text_feature, video_feature)
-            # output = output.squeeze(dim=1)
-            loss = criterion(output, label)
+            loss = criterion(output, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if n % 50 == 0 and n != 0:
-                print("Epoch {}, batch {}: loss = {}".format(epoch, n, running_loss / 50))
+            if n % 50 == 49:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, n + 1, running_loss / 50))
                 running_loss = 0
     return model
 
@@ -89,37 +40,67 @@ def evaluation(test_data, model):
     total_num = 0
 
     for data in test_data:
-        text_feature, video_feature, label = data
-
+        text_feature, video_feature, labels = data
         text_feature = text_feature.cuda()
         video_feature = video_feature.cuda()
-        label = label.cuda()
+        _, labels = labels.max(dim=1)
+        labels = labels.cuda()
 
         output = model(text_feature, video_feature)
+        output = output.squeeze(dim=1)
         batch_size = output.shape[0]
         for i in range(batch_size):
-            if torch.argmax(output[i]).item() == label[i].item():
+            if torch.argmax(output[i]).item() == labels[i].item():
                 correct += 1
             total_num += 1
 
     acc = correct / total_num
-    print(acc)
+    print(f'accuracy: {acc}')
 
 
-print("start")
+folder = '/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions'
+video_features_file = f'{folder}/ActivityNet_Captions_Video_Features/sub_activitynet_v1-3.c3d.hdf5'
+train_text_file = f'{folder}/train.json'
 
-trainDataset = ActivityNetCaptionsDataset(trainTextFile, videoFeatures)
-trainLoader = DataLoader(trainDataset, batch_size=32, shuffle=True, num_workers=4)
-print("successfully load train")
+bow_representation_train_file = f'{folder}/bow_training_data.pkl'
+bow_representation_val_file = f'{folder}/bow_validation_data.pkl'
+masked_training_data_file = f'{folder}/train'
+masked_validation_data_file = f'{folder}/val1'
 
-valDataset = ActivityNetCaptionsDataset(valTextFile, videoFeatures, isTrain=False)
-valLoader = DataLoader(valDataset, batch_size=16, shuffle=True, num_workers=4)
-print("successfully load val")
+num_tokens = 1000
 
-model = baseline_BOW_VF(1000, 500, len(word_dict)).cuda()
+print('start constructing bag of word representations for training and validation using current setting')
+print(f'num_tokens = {num_tokens}, which represents the number of most frequent words used in text representations')
+print(f'a list of tuples (sentence representation, video feature, label) '
+      f'\n-- training data will be saved in {bow_representation_train_file},'
+      f'\n-- validation data will be saved in {bow_representation_val_file}')
+
+build_representation(num_tokens, masked_training_data_file, train_text_file,
+                     video_features_file, bow_representation_train_file)
+build_representation(num_tokens, masked_validation_data_file, train_text_file,
+                     video_features_file, bow_representation_val_file)
+
+print('loading training data...')
+trainDataset = ActivityNetCaptionsDataset(bow_representation_train_file)
+trainLoader = DataLoader(trainDataset, batch_size=32, shuffle=True, num_workers=0)
+print("successfully loaded training data")
+
+valDataset = ActivityNetCaptionsDataset(bow_representation_val_file)
+valLoader = DataLoader(valDataset, batch_size=16, shuffle=True, num_workers=0)
+print("successfully loaded validation data")
+
+_, num_vocabs = fit(train_text_file, 1000)
+print(f"num_vocabs: {num_vocabs}")
+model = baseline_BOW_VF(1000, 500, num_vocabs).cuda()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
 max_epoch = 30
 
 model = train(trainLoader, max_epoch, model, optimizer, criterion)
 evaluation(valLoader, model)
+
+model_file = f'{folder}/baseline_bow_average_video_feature_model.pkl'
+model_pickle_file = open(model_file, 'wb')
+pickle.dump(model, model_pickle_file)
+model_pickle_file.close()
+print(f"successfully saved the model in {model_file}")
