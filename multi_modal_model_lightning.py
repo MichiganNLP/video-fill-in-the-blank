34ff7fcd629d7bceda1c44e74612dd52f4bf87be
Ruoyao Wang <wsxzwps43@126.com>
2020-06-26 01:06:12 -0400

    debug

--- multi_modal_model_lightning.py
+++ multi_modal_model_lightning.py
@@ -176,13 +176,14 @@ def _main() -> None:
 
         for batch in data:
             batch_size = batch[0][0].shape[0]
-            text_token_ids, visual, mask, segment_mask, labels, mask_positions, mask_lm_labels, position_ids, standard_answers = batch[0]
+            text_token_ids, visual, mask, segment_mask, labels, mask_positions, mask_lm_labels, position_ids, standard_answers, extended_answers = batch[0]
             out = model(text_token_ids, visual, mask, segment_mask, mask_lm_labels, position_ids)
             loss, scores = out
             prediction_indices = torch.argmax(scores[list(range(batch_size)), mask_positions], dim=1)
 
             predictions = tokenizer.convert_ids_to_tokens(prediction_indices.tolist())
-            extended_results = [(prediction in label) for prediction, label in zip(predictions, labels.keys())]
+            worker_results = [(prediction in label.keys()) for prediction, label in zip(predictions, labels)]
+            extended_results = [(prediction in label) for prediction, label in zip(predictions, extended_answers)]
             standard_results = [(prediction == label) for prediction, label in zip(predictions, standard_answers)]
             # for i in range(len(extended_results)):
             #     if extended_results[i] != standard_results[i]:
