89fd2ce8aad865eddbc035f4211da3f967e2b0a6
Ruoyao Wang <wsxzwps43@126.com>
2020-06-25 20:39:09 -0400

    debug

--- data_loader_update.py
+++ data_loader_update.py
@@ -42,7 +42,7 @@ def gen(masked_data_file, text_file, video_features):
                 start_frame = math.floor(tt_start / duration * video_feature_len)
                 end_frame  = math.floor(tt_end / duration * video_feature_len)
 
-                video_feature_np = videoFeatures[key]['c3d_features'][startFrame:endFrame+1]
+                video_feature_np = video_features[key]['c3d_features'][startFrame:endFrame+1]
         
                 if vidoe_feature_np.shape[0] > 200:
                     video_feature = np.zeros((200, video_feature_np.shape[1]))
