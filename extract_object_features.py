1876e3d894dc717b43d241c425834c2fc46aad89
Ruoyao Wang <wsxzwps43@126.com>
2020-08-05 16:26:55 -0400

    debug object feature extraction

--- extract_object_features.py
+++ extract_object_features.py
@@ -4,10 +4,12 @@ import numpy as np
 import torch
 import os
 
+folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions/activitynet_frames/"
+
 def getObjectFeature(self, input, output):
     print(output.data)
 
-image = Image.open('v_qkN9uA8izVE/000001.jpg')
+image = Image.open(f'{folder}v_qkN9uA8izVE/000001.jpg')
 img_np = np.asarray(image) / 255
 img_tensor = torch.FloatTensor(img_np)
 img_tensor = img_tensor.permute(2, 0, 1)
