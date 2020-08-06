f59960a796d84f8931a62167b7dacd73207a7eb5
Ruoyao Wang <wsxzwps43@126.com>
2020-08-05 22:27:04 -0400

    debug object feature extraction

--- extract_object_features.py
+++ extract_object_features.py
@@ -5,18 +5,28 @@ import torch
 import os
 
 folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions/activitynet_frames/"
-features = []
+features = {}
 def getObjectFeature(self, input, output):
-    features.append(output.data)
+    global feature
+    feature = output.data
 
-image = Image.open(f'{folder}v_2mI7NL54yP8/000001.jpg')
-img_np = np.asarray(image) / 255
-img_tensor = torch.FloatTensor(img_np)
-img_tensor = img_tensor.permute(2, 0, 1)
+def getPrediction(self, input, output):
+    global scores
+    scores = output.data
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-model.roi_heads.box_roi_pool.register_forward_hook(getObjectFeature)
-model.eval()
+for video in os.listdir(folder):
+    frame_num = len(os.listdir(f"{folder}{video}"))
+    for i in range(frame_num):
+        frame_name = '0' * (6-len(str(i + 1))) + str(i + 1) + '.jpg'
+        image = Image.open(f'{folder}{video}/{frame_name}')
+        img_np = np.asarray(image) / 255
+        img_tensor = torch.FloatTensor(img_np)
+        img_tensor = img_tensor.permute(2, 0, 1)
 
-pred = model([img_tensor, img_tensor])
-pass
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        model.roi_heads.box_roi_pool.register_forward_hook(getObjectFeature)
+        model.roi_heads.box_predictor.cls_score.register_forward_hook(getPrediction)
+        model.eval()
+
+        pred = model([img_tensor, img_tensor])
+        pass
