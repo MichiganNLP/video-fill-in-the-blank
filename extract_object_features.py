4a58ed4d7a521e8f8cd86de939bce48dc52c41d0
Ruoyao Wang <wsxzwps43@126.com>
2020-08-05 23:30:11 -0400

    debug object feature extraction

--- extract_object_features.py
+++ extract_object_features.py
@@ -6,16 +6,20 @@ import os
 
 folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions/activitynet_frames/"
 features = {}
+THESHROLD = 10
 def getObjectFeature(self, input, output):
     global feature
     feature = output.data
 
 def getPrediction(self, input, output):
     global scores
-    scores = output.data
+    global cl
+
+    scores, cl = torch.max(output.data)
 
 for video in os.listdir(folder):
     frame_num = len(os.listdir(f"{folder}{video}"))
+    features[video] = []
     for i in range(frame_num):
         frame_name = '0' * (6-len(str(i + 1))) + str(i + 1) + '.jpg'
         image = Image.open(f'{folder}{video}/{frame_name}')
@@ -27,6 +31,10 @@ for video in os.listdir(folder):
         model.roi_heads.box_roi_pool.register_forward_hook(getObjectFeature)
         model.roi_heads.box_predictor.cls_score.register_forward_hook(getPrediction)
         model.eval()
-
-        pred = model([img_tensor, img_tensor])
-        pass
+        pred = model([img_tensor])
+        # select top THRESHOLD
+        _, idx = torch.topk(scores, THESHROLD)
+        feature = torch.index_select(feature, 0, idx)
+        # scores = torch.index_select(scores, 0, idx)
+        cl = torch.index_select(cl, 0, idx)
+        features.append([feature, cl])
