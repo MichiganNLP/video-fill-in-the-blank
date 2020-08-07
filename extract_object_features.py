00897acea17d1f4c64eb9eba8989fb457516ec1d
Ruoyao Wang <wsxzwps43@126.com>
2020-08-06 21:30:33 -0400

    debug od

--- extract_object_features.py
+++ extract_object_features.py
@@ -1,13 +1,17 @@
 import torchvision
+from torchvision.ops import boxes as box_ops
+import torch.nn.functional as F
 from PIL import Image
 import numpy as np
 import torch
 import os
 import pickle
+from object_detect_utils import BoxCoder
 
 folder = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions/activitynet_frames/"
 features = {}
 THESHROLD = 16
+box_score_thresh = 0.05
 def getObjectFeature(self, input, output):
     global feature
     feature = output.data
@@ -21,8 +25,69 @@ def getBBox(self, input, output):
     global bbox
     bbox = output.data
 
-def debug(self, input, output):
+def ROIHeads_BoxPredictorHook(self, input, output):
     pass
+
+def postprocess_detections(class_logits,    # type: Tensor
+                               box_regression,  # type: Tensor
+                               proposals,       # type: List[Tensor]
+                               image_shapes     # type: List[Tuple[int, int]]
+                               ):
+        # type: (...) -> Tuple[List[Tensor], List[Tensor], List[Tensor]]
+        device = class_logits.device
+        num_classes = class_logits.shape[-1]
+        
+        bbox_reg_weights = (10., 10., 5., 5.)
+        box_coder = BoxCoder(bbox_reg_weights)
+
+        bbox_reg_weights = (10., 10., 5., 5.)
+        boxes_per_image = [boxes_in_image.shape[0] for boxes_in_image in proposals]
+        pred_boxes = box_coder.decode(box_regression, proposals)
+
+        pred_scores = F.softmax(class_logits, -1)
+
+        pred_boxes_list = pred_boxes.split(boxes_per_image, 0)
+        pred_scores_list = pred_scores.split(boxes_per_image, 0)
+
+        all_boxes = []
+        all_scores = []
+        all_labels = []
+        for boxes, scores, image_shape in zip(pred_boxes_list, pred_scores_list, image_shapes):
+            boxes = box_ops.clip_boxes_to_image(boxes, image_shape)
+
+            # create labels for each prediction
+            labels = torch.arange(num_classes, device=device)
+            labels = labels.view(1, -1).expand_as(scores)
+
+            # remove predictions with the background label
+            boxes = boxes[:, 1:]
+            scores = scores[:, 1:]
+            labels = labels[:, 1:]
+
+            # batch everything, by making every class prediction be a separate instance
+            boxes = boxes.reshape(-1, 4)
+            scores = scores.reshape(-1)
+            labels = labels.reshape(-1)
+
+            # remove low scoring boxes
+            inds = torch.nonzero(scores > self.score_thresh).squeeze(1)
+            boxes, scores, labels = boxes[inds], scores[inds], labels[inds]
+
+            # remove empty boxes
+            keep = box_ops.remove_small_boxes(boxes, min_size=1e-2)
+            boxes, scores, labels = boxes[keep], scores[keep], labels[keep]
+
+            # non-maximum suppression, independently done per class
+            keep = box_ops.batched_nms(boxes, scores, labels, self.nms_thresh)
+            # keep only topk scoring predictions
+            keep = keep[:self.detections_per_img]
+            boxes, scores, labels = boxes[keep], scores[keep], labels[keep]
+
+            all_boxes.append(boxes)
+            all_scores.append(scores)
+            all_labels.append(labels)
+
+        return all_boxes, all_scores, all_labels
 
 for video in os.listdir(folder):
     frame_num = len(os.listdir(f"{folder}{video}"))
@@ -38,7 +103,7 @@ for video in os.listdir(folder):
         model.roi_heads.box_roi_pool.register_forward_hook(getObjectFeature)
         model.roi_heads.box_predictor.cls_score.register_forward_hook(getPrediction)
         model.roi_heads.box_predictor.bbox_pred.register_forward_hook(getBBox)
-        model.roi_heads.box_predictor.register_forward_hook(debug)
+        model.roi_heads.box_predictor.register_forward_hook(ROIHeads_BoxPredictorHook)
         model.eval()
         pred = model([img_tensor])
         # select top THRESHOLD
