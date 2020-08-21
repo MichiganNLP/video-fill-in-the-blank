7478ace3da78a041aad1816a7a3002492d64c224
Ruoyao Wang <wsxzwps43@126.com>
2020-08-20 23:55:16 -0400

    val and test

--- extract_object_features.py
+++ extract_object_features.py
@@ -117,7 +117,7 @@ for video in os.listdir(folder):
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
         model.roi_heads.register_forward_hook(ROIHeadsHook)
         model.roi_heads.box_predictor.register_forward_hook(ROIHeads_BoxPredictorHook)
-        model.rpn.head.cls_logits.register_forward_hook(RPN_ClslogitsHook)
+        model.rpn.register_forward_hook(RPN_ClslogitsHook)
         model.eval()
         pred = model([img_tensor])
         # # select top THRESHOLD
