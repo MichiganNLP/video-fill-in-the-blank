1050b8af8310b2eea029d23bb6f1a18d4e736023
Ruoyao Wang <wsxzwps43@126.com>
2020-09-12 19:52:53 -0400

    to cpu

--- great_lakes/submit_job_tocpu.sh
+++ great_lakes/submit_job_tocpu.sh
@@ -8,14 +8,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=10000m 
+#SBATCH --gres=gpu:1
 #SBATCH --time=01-5:00:00
 #SBATCH --account=mihalcea1
-#SBATCH --partition=standard
+#SBATCH --partition=gpu
 # The application(s) to execute along with its input arguments and options:
 
 # Use your own conda because Great Lakes ones are old and thus problematic.
 # source ~/.bashrc
 eval "$(conda shell.bash hook)"
 conda activate lqam
-python -u extract_object_features.py
-
+python -u normal_bbox.py
