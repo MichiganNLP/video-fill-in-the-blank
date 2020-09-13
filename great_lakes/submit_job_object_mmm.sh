c46cc2e65708b9b1af1a6c9283b9c43017516038
Ruoyao Wang <wsxzwps43@126.com>
2020-09-12 23:05:10 -0400

    object detection model experiment

--- great_lakes/submit_job_object_mmm.sh
+++ great_lakes/submit_job_object_mmm.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=10000m 
 #SBATCH --gres=gpu:1
-#SBATCH --time=01-5:00:00
+#SBATCH --time=02-5:00:00
 #SBATCH --account=mihalcea1
 #SBATCH --partition=gpu
 # The application(s) to execute along with its input arguments and options:
