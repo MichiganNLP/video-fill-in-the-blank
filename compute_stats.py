16b45e549dcf484c808a96bebcea0961fb832ad8
Ruoyao Wang <wsxzwps43@126.com>
2020-10-16 11:12:02 -0400

    eval

--- compute_stats.py
+++ compute_stats.py
@@ -7,7 +7,7 @@ word_dict = {}
 more_than_one_token = 0
 for t in train_data:
     label = t[2]
-    if len(label) > 0:
+    if len(label) > 1:
         more_than_one_token += 1
 
 print(more_than_one_token)
