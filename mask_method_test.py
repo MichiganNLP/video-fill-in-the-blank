c1cfc9c67767ad05f5a060dd8321d6e1498f41ae
Ruoyao Wang <wsxzwps43@126.com>
2020-10-27 22:37:49 -0400

    set up mask method test

---
+++ mask_method_test.py
@@ -0,0 +1,14 @@
+import json
+import torch
+from transformers import BertModel, BertTokenizer
+
+JSON_FILE = "vatex_training_v1.0.json"
+with open(JSON_FILE, 'r') as f:
+    data = json.load(f)
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+for caption in data:
+    sentence = caption["enCap"][0] # just pick the first one
+    bert_out = model(**tokenizer(sentence))
