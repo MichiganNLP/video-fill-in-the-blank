cc16070984fd292fbf7cd35f65a6b843c3dfc052
Ruoyao Wang <wsxzwps43@126.com>
2020-12-02 23:56:14 -0500

    add progress bar

--- generate_VATEX_data.py
+++ generate_VATEX_data.py
@@ -6,7 +6,7 @@ import pickle
 from tqdm.auto import tqdm
 
 DATA_FILE_FOLDER = "/scratch/mihalcea_root/mihalcea1/shared_data/ActivityNet_Captions/latest_data/multimodal_model/VATEX"
-DIV_NAME = "test"
+DIV_NAME = "val"
 
 data = []
 
