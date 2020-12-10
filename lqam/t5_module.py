e70497a9eb000f1cdf980a14b296cb61edbb8f52
Santiago Castro <santi.1410@hotmail.com>
2020-12-10 01:45:21 -0300

    WIP: t5 loss

--- lqam/t5_module.py
+++ lqam/t5_module.py
@@ -1,29 +1,36 @@
-from typing import Optional
+from typing import Any, Mapping, Optional
 
 import pytorch_lightning as pl
 import torch
 from transformers import MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING, PreTrainedModel
+from transformers.modeling_outputs import Seq2SeqLMOutput
 
 from lqam.data_module import TYPE_BATCH
 
 
 class T5Filler(pl.LightningModule):
-    def __init__(self, t5_like_pretrained_model: PreTrainedModel) -> None:
+    def __init__(self, t5_like_pretrained_model: PreTrainedModel,
+                 generate_kwargs: Optional[Mapping[str, Any]] = None) -> None:
         super().__init__()
+        # TODO: hparams
         # The model doesn't necessarily use T5 classes (e.g., `T5PreTrainedModel`).
         # It just needs to be pretrained like T5 and support conditional generation.
         assert isinstance(t5_like_pretrained_model, tuple(MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING.values()))
         self.t5_pretrained_model = t5_like_pretrained_model
+        self.generate_kwargs = generate_kwargs or {}
 
-    def forward(self, masked_caption_ids: torch.Tensor, label_ids: Optional[torch.Tensor] = None,
-                **kwargs) -> torch.Tensor:
-        return self.t5_pretrained_model.generate(masked_caption_ids, **kwargs)
+    def forward(self, masked_caption_ids: torch.Tensor, label_ids: Optional[torch.Tensor] = None) -> Seq2SeqLMOutput:
+        return self.t5_pretrained_model(masked_caption_ids, labels=label_ids, **self.generate_kwargs)
 
-    def _generative_step(self, batch: TYPE_BATCH):
-        return self(**batch)
+    def _step(self, batch: TYPE_BATCH) -> float:
+        outputs = self(**batch)
+        return outputs["loss"]
 
-    def validation_step(self, batch: TYPE_BATCH, batch_idx: int = 0) -> torch.Tensor:
+    def _generative_step(self, batch: TYPE_BATCH) -> float:
+        return self._step(batch)
+
+    def validation_step(self, batch: TYPE_BATCH, batch_idx: int = 0) -> float:
         return self._generative_step(batch)
 
-    def test_step(self, batch: TYPE_BATCH, batch_idx: int = 0) -> torch.Tensor:
+    def test_step(self, batch: TYPE_BATCH, batch_idx: int = 0) -> float:
         return self._generative_step(batch)
