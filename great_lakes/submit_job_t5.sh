1588bd43c0033e137f2dc93879e748b13123ff75
Ruoyao Wang <wsxzwps43@126.com>
2020-12-11 05:27:57 -0500

    add script

---
+++ great_lakes/submit_job_t5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=lightning_mm_t5_model
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=10000m 
+#SBATCH --gres=gpu:1
+#SBATCH --time=02-5:00:00
+#SBATCH --account=mihalcea1
+#SBATCH --partition=gpu
+# The application(s) to execute along with its input arguments and options:
+echo Started!
+# Use your own conda because Great Lakes ones are old and thus problematic.
+# source ~/.bashrc
+echo Hooking
+eval "$(conda shell.bash hook)"
+echo Sourcing
+conda activate lqam
+echo Sourced
+python -u ../multi_modal_model_t5.py --data-path /scratch/mihalcea_root/mihalcea1/shared_data/qgen/VATEX/multimodal_model/VATEX/ --num-workers 8
+echo done
