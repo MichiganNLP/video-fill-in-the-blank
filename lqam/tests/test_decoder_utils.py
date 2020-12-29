4772c8f84555425236409df3d958ef572ee89cc3
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2020-12-29 13:55:38 -0500

    Added test case.

---
+++ lqam/tests/test_decoder_utils.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from lqam.decoder_utils import compute_noun_phrase_mask
+
+import torch
+import spacy
+
+
+class TestComputeNounPhraseMask(TestCase):
+    def test_compute_noun_phrase_mask(self):
+        nlp = spacy.load("en_core_web_sm")
+        phrases = ['a dog', 'hello', 'sad', 'happy']
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        mask = compute_noun_phrase_mask(nlp, phrases, batch_size=2, num_return_sequences=2, device=device)
+        answer = [[True, False], [True, False]]
+        self.assertListEqual(mask.tolist(), answer)
