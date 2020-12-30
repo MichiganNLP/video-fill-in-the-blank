c84e94f8b254b309fbc4b32847464047a472c8f6
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2020-12-30 17:25:04 -0500

    Fixed the test case.

--- lqam/tests/test_decoder_utils.py
+++ lqam/tests/test_decoder_utils.py
@@ -11,5 +11,5 @@ class TestComputeNounPhraseMask(TestCase):
         phrases = ['a dog', 'hello', 'sad', 'happy']
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         mask = compute_noun_phrase_mask(nlp, phrases, batch_size=2, num_return_sequences=2, device=device)
-        answer = [[True, False], [True, False]]
+        answer = [True, False, True, False]
         self.assertListEqual(mask.tolist(), answer)
