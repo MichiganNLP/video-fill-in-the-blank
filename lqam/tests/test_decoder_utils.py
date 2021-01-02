dee541940d89586aae2a514591016f19ff5ed808
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2021-01-02 11:47:46 -0500

    Update test code for the noun phrase helper function.

--- lqam/tests/test_decoder_utils.py
+++ lqam/tests/test_decoder_utils.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from lqam.decoder_utils import compute_noun_phrase_mask
+from lqam.decoder_utils import compute_noun_phrase_indices
 
 import torch
 import spacy
@@ -8,8 +8,10 @@ import spacy
 class TestComputeNounPhraseMask(TestCase):
     def test_compute_noun_phrase_mask(self):
         nlp = spacy.load("en_core_web_sm")
-        phrases = ['a dog', 'hello', 'sad', 'happy']
+        phrases = ['a dog and', 'hello',
+                   'sad', 'happy',
+                   'a cat and', 'a dog']
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        mask = compute_noun_phrase_mask(nlp, phrases, batch_size=2, num_return_sequences=2, device=device)
-        answer = [True, False, True, False]
+        mask = compute_noun_phrase_indices(nlp, phrases, batch_size=2, num_return_sequences=2, device=device)
+        answer = [2, 5]
         self.assertListEqual(mask.tolist(), answer)
