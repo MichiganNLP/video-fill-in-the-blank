89b675880e5886ed9ebc8cb5bed573fdb4aee834
Santiago Castro <santi.1410@hotmail.com>
2021-01-11 19:47:31 -0300

    Change cache folder to a more standard one

--- lqam/util/file_utils.py
+++ lqam/util/file_utils.py
@@ -31,8 +31,8 @@ from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
-CACHE_ROOT = Path(os.getenv("ADS_CACHE_ROOT", Path.home() / ".lqam"))
-CACHE_DIRECTORY = str(CACHE_ROOT / "cache")
+CACHE_ROOT = Path(os.getenv("LQAM_CACHE_ROOT", Path.home() / ".cache"))
+CACHE_DIRECTORY = str(CACHE_ROOT / "lqam")
 DEPRECATED_CACHE_DIRECTORY = str(CACHE_ROOT / "datasets")
 
 # This variable was deprecated in 0.7.2 since we use a single folder for caching
