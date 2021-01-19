f407bc5cfc7f54b48ea04e1a2b3458e6a5cca005
Ruoyao Wang <wsxzwps43@126.com>
2021-01-19 12:35:46 -0500

    debug

--- lqam/t5_multi_modal_module.py
+++ lqam/t5_multi_modal_module.py
@@ -33,5 +33,8 @@ class T5AndI3D(T5ForConditionalGeneration):
     
     @overrides
     def forward(self, masked_caption_ids, visual = None, labels = None, *args, **kwargs):
-        encoder_output = self.encoder(masked_caption_ids, visual=visual)
+        if "encoder_outputs" in kwargs:
+            encoder_output = kwargs["encoder_outputs"]
+        else:
+            encoder_output = self.encoder(masked_caption_ids, visual=visual)
         return super().forward(encoder_outputs=encoder_output, labels = labels, return_dict = True, *args, **kwargs)
