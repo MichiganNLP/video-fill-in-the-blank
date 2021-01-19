76c10f12333ec55092c08b3b753424a1a0616b3a
Ruoyao Wang <wsxzwps43@126.com>
2021-01-19 11:15:24 -0500

    debug

--- evaluate_multi_modal_baseline.py
+++ evaluate_multi_modal_baseline.py
@@ -52,6 +52,7 @@ def main() -> None:
     data_module = QGenDataModule(tokenizer=tokenizer, batch_size=args.batch_size, num_workers=args.num_workers, hasVisual=True)
 
     t5_like_pretrained_model = T5AndI3D.from_pretrained(args.model, visual_size=args.visual_size)
+    t5_like_pretrained_model.setEncoder()
     filler = T5FillerModel(t5_like_pretrained_model=t5_like_pretrained_model, tokenizer=tokenizer,
                            only_noun_phrases=args.only_noun_phrases,
                            visual_size = args.visual_size,
