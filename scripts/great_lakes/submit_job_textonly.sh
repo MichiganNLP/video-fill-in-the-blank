84bfc73b8eb71d8d084dfbe78622eeb63dda59ae
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2021-01-22 20:45:02 -0500

    Update textonly experiment scripts.

--- scripts/great_lakes/submit_job_textonly.sh
+++ scripts/great_lakes/submit_job_textonly.sh
@@ -5,13 +5,15 @@
 #SBATCH --job-name=textonly
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=4
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=8000m 
+#SBATCH --mem-per-cpu=2gb
+#SBATCH --ntasks=4
+#SBATCH --tasks-per-node=4
 #SBATCH --gres=gpu:1
-#SBATCH --time=05-5:00:00
+#SBATCH --time=4-0:00:00
 #SBATCH --account=mihalcea1
 #SBATCH --partition=gpu
+#SBATCH --output=LifeQA-methodology/textonly.out
+#SBATCH --error=LifeQA-methodology/textonly.err
 # The application(s) to execute along with its input arguments and options:
 echo Started!
 # Use your own conda because Great Lakes ones are old and thus problematic.
@@ -21,5 +23,69 @@ eval "$(conda shell.bash hook)"
 echo Sourcing
 conda activate lqam
 echo Sourced
-python -u train_text_only.py
+cd LifeQA-methodology || exit
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4
+echo Greedy - done!
+
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 2 and --generation-early-stopping flag is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 4 and --generation-early-stopping flag is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 8 and --generation-early-stopping flag is not set - done!
+
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--generation-early-stopping --num-workers=4 --only-noun-phrases
+echo Beam Size = 2 and --no-repeat-ngram-size flag is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--generation-early-stopping --num-workers=4 --only-noun-phrases
+echo Beam Size = 4 and --no-repeat-ngram-size flag is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--generation-early-stopping --num-workers=4 --only-noun-phrases
+echo Beam Size = 8 and --no-repeat-ngram-size flag is not set - done!
+
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 2 - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 4 - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4 --only-noun-phrases
+echo Beam Size = 8 - done!
+
+# NO NP FILTERS
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 2 and --generation-early-stopping flag --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 4 and --generation-early-stopping flag --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 8 and --generation-early-stopping flag --only-noun-phrases is not set - done!
+
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--generation-early-stopping --num-workers=4
+echo Beam Size = 2 and --no-repeat-ngram-size flag --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--generation-early-stopping --num-workers=4
+echo Beam Size = 4 and --no-repeat-ngram-size flag --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--generation-early-stopping --num-workers=4
+echo Beam Size = 8 and --no-repeat-ngram-size flag --only-noun-phrases is not set - done!
+
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=2 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 2 --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=4 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 4 --only-noun-phrases is not set - done!
+python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --beam-size=8 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4
+echo Beam Size = 8 --only-noun-phrases is not set - done!
 echo "done"
