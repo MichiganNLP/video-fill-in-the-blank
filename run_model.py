9f088f978eb66d0ad1dbb644536d9399064de25a
Ruoyao Wang <wsxzwps43@126.com>
2021-02-02 15:10:09 -0500

    add random seeds; default test; change data path args

--- run_model.py
+++ run_model.py
@@ -9,15 +9,19 @@ import os
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 from lqam.util.argparse_with_defaults import ArgumentParserWithDefaults
-from lqam.methods.dataset import QGenDataModule
+from lqam.methods.dataset import QGenDataModule, URL_DATA_TRAIN, URL_DATA_VAL, URL_DATA_TEST
 from lqam.methods.t5_filler_model import T5FillerModel
 from lqam.methods.t5_multi_modal_module import T5AndVisual
 
 def _parse_args() -> argparse.Namespace:
     parser = ArgumentParserWithDefaults(description="Evaluate the T5 multi-modal baseline.")
 
-    parser.add_argument("--data-path", default="https://drive.google.com/uc?id=1-JRsjFzP3Qmjti_w8ILV06msXjw4OXoB"
-                                               "&export=download")
+    # Data paths
+    parser.add_argument("--train-data-path", default=URL_DATA_TRAIN)
+    parser.add_argument("--val-data-path", default=URL_DATA_VAL)
+    parser.add_argument("--test-data-path", default=URL_DATA_TEST)
+    parser.add_argument("--visual-data-path", default="/scratch/mihalcea_root/mihalcea1/shared_data/qgen/VATEX/I3D_video_features")
+
     parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--num-workers", "-j", type=int, default=0,
                         help="data loader workers. Each worker batch-tokenizes in parallel, "
@@ -41,6 +45,11 @@ def _parse_args() -> argparse.Namespace:
     parser.add_argument("--no-repeat-ngram-size", type=int)
     parser.add_argument("--only-noun-phrases", action="store_true")
 
+    # enable reproducibility
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--no-benchmark", dest="benchmark", action="store_false")
+    parser.add_argument("--no-deterministic", dest="deterministic", action="store_false")
+
     parser.add_argument("--predictions-output-path", default="predictions.csv")
     parser.add_argument("--beta1", default=0.9, type=float, help="beta1 for the Adam optimizer")
     parser.add_argument("--beta2", default=0.999, type=float, help="beta2 for the Adam optimizer")
@@ -52,12 +61,21 @@ def _parse_args() -> argparse.Namespace:
     parser.add_argument("--fast-dev-run", action="store_true")
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--has-visual", action="store_true")
-    parser.add_argument("--visual-data-path", default="/scratch/mihalcea_root/mihalcea1/shared_data/qgen/VATEX/I3D_video_features")
+    
     return parser.parse_args()
 
+def _pandas_float_format(x: float) -> str:
+    if x == 0:
+        return "0"
+    elif abs(x) < 0.005:
+        return np.format_float_scientific(x, exp_digits=1, precision=0, trim="-")
+    else:
+        return f"{x:.2f}"
 
 def main() -> None:
     args = _parse_args()
+
+    pl.seed_everything(args.seed)
 
     tokenizer = AutoTokenizer.from_pretrained(args.model)
     data_module = QGenDataModule(tokenizer=tokenizer, batch_size=args.batch_size, num_workers=args.num_workers, hasVisual=args.has_visual)
@@ -91,13 +109,14 @@ def main() -> None:
     if args.has_visual:
         visual_data_path = args.visual_data_path
 
-    train_dataloaders = data_module.train_dataloader(visual_data_path = visual_data_path)
-    val_dataloaders = data_module.val_dataloader(visual_data_path = visual_data_path)
-    test_dataloaders=data_module.test_dataloader( visual_data_path = visual_data_path)
-    trainer = pl.Trainer(gpus=args.gpus, default_root_dir=args.default_root_dir, fast_dev_run = args.fast_dev_run, max_epochs = args.epochs)
+    train_dataloaders = data_module.train_dataloader(data_path = args.train_data_path, visual_data_path = visual_data_path)
+    val_dataloaders = data_module.val_dataloader(data_path = args.val_data_path, visual_data_path = visual_data_path)
+    # test_dataloaders=data_module.test_dataloader(data_path = args.test_data_path, visual_data_path = visual_data_path)
+    trainer = pl.Trainer(gpus=args.gpus, default_root_dir=args.default_root_dir, fast_dev_run = args.fast_dev_run,
+                         max_epochs = args.epochs, benchmark=args.benchmark, deterministic=args.deterministic)
     if args.train:
         trainer.fit(filler, train_dataloaders, val_dataloaders)
-    trainer.test(filler, test_dataloaders=test_dataloaders)
+    trainer.test(filler, test_dataloaders=val_dataloaders)
 
     predictions = {k: v.tolist() if isinstance(v, torch.Tensor) else v
                    for k, v in next(iter(trainer.evaluation_loop.predictions.predictions.values())).items()}
@@ -105,9 +124,10 @@ def main() -> None:
     df.to_csv(args.predictions_output_path, index=False)
     print(f"Predictions saved in {args.predictions_output_path}. First rows:")
     print()
-    pd.options.display.float_format = \
-        lambda x: np.format_float_scientific(x, exp_digits=1, precision=0, trim="-") if abs(x) < 0.005 else f"{x:.2f}"
-    print(df.head(10))
+    pd.options.display.float_format = _pandas_float_format
+    with pd.option_context("display.max_rows", None, "display.max_columns", None, "display.width", 0,
+                           "display.max_colwidth", None):
+        print(df.head(10))
 
 
 if __name__ == "__main__":
