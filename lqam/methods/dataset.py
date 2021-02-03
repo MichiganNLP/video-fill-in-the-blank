26143ad6d8582816988b6eb572bb890ef57a284d
Ruoyao Wang <wsxzwps43@126.com>
2021-02-02 20:30:17 -0500

    merge two collate_fn

--- lqam/methods/dataset.py
+++ lqam/methods/dataset.py
@@ -82,54 +82,81 @@ class QGenDataset(Dataset):
             tokenization_output = self.tokenizer(to_tokenize, padding="longest", truncation=True, return_tensors="pt")
             batch[f"{k}_ids"] = tokenization_output["input_ids"]
             batch[f"{k}_attention_mask"] = tokenization_output["attention_mask"]
+        if "visual" in instances:
+            batch_size = len(instances)
+            max_video_len = 0
+            video_features = []
+            for i in range(batch_size):
+                video_features.append(instances[i])
+                total_video_len = data["visual"].shape[0]
+
+                if total_video_len > max_video_len:
+                    max_video_len = total_video_len
+
+            video_tensor = torch.zeros(batch_size, max_video_len, visual_size, dtype=torch.float)
+            video_attention_mask = torch.zeros(batch_size, max_video_len, dtype=torch.long)
+
+            for i in range(batch_size):
+                video = video_features[i]
+                video_len = len(video)
+
+                # The input to the transformer is gonna be:
+                # t_1 ... t_n pad ... pad </s> v_1 ... v_m pad ... pad
+
+                video_len = video.shape[0]
+                video_tensor[i, :video_len] = video
+                video_attention_mask[i, :video_len] = True
+            batch['visual'] = video_tensor
+            batch['masked_caption_attention_mask'] = torch.cat([batch['masked_caption_attention_mask'], video_attention_mask], 1)
+
         return batch
 
-    def collate_fn_multi_modal(self, batch: Sequence[Sequence[Any]]) -> TYPE_BATCH:
-        batch_size = len(batch)
-        masked_captions = []
-        video_features = []
-        label_list = []
+    # def collate_fn_multi_modal(self, batch: Sequence[Sequence[Any]]) -> TYPE_BATCH:
+    #     batch_size = len(batch)
+    #     masked_captions = []
+    #     video_features = []
+    #     label_list = []
 
-        max_video_len = 0
-        visual_size = batch[0]["visual"].shape[1]
-        for i in range(batch_size):
-            data = batch[i]
-            masked_captions.append(data["masked_caption"])
-            video_features.append(data["visual"])
-            label_list.append(data["label"])
+    #     max_video_len = 0
+    #     visual_size = batch[0]["visual"].shape[1]
+    #     for i in range(batch_size):
+    #         data = batch[i]
+    #         masked_captions.append(data["masked_caption"])
+    #         video_features.append(data["visual"])
+    #         label_list.append(data["label"])
 
-            total_video_len = data["visual"].shape[0]
+    #         total_video_len = data["visual"].shape[0]
 
-            if total_video_len > max_video_len:
-                max_video_len = total_video_len
+    #         if total_video_len > max_video_len:
+    #             max_video_len = total_video_len
 
-        label_with_special_tokens = [f"<extra_id_0> {label} <extra_id_1>" for label in label_list]
-        text_batch = self.tokenizer.prepare_seq2seq_batch(src_texts=masked_captions, tgt_texts=label_with_special_tokens,
-                                                          padding=True, return_tensors="pt")
-        text_tensor = text_batch.input_ids
-        text_attention_mask = text_batch.attention_mask
-        labels = text_batch.labels
+    #     label_with_special_tokens = [f"<extra_id_0> {label} <extra_id_1>" for label in label_list]
+    #     text_batch = self.tokenizer.prepare_seq2seq_batch(src_texts=masked_captions, tgt_texts=label_with_special_tokens,
+    #                                                       padding=True, return_tensors="pt")
+    #     text_tensor = text_batch.input_ids
+    #     text_attention_mask = text_batch.attention_mask
+    #     labels = text_batch.labels
 
-        video_tensor = torch.zeros(batch_size, max_video_len, visual_size, dtype=torch.float)
+    #     video_tensor = torch.zeros(batch_size, max_video_len, visual_size, dtype=torch.float)
 
-        video_attention_mask = torch.zeros(batch_size, max_video_len, dtype=torch.long)
+    #     video_attention_mask = torch.zeros(batch_size, max_video_len, dtype=torch.long)
 
-        for i in range(batch_size):
-            video = video_features[i]
-            video_len = len(video)
+    #     for i in range(batch_size):
+    #         video = video_features[i]
+    #         video_len = len(video)
 
-            # The input to the transformer is gonna be:
-            # t_1 ... t_n pad ... pad </s> v_1 ... v_m pad ... pad
+    #         # The input to the transformer is gonna be:
+    #         # t_1 ... t_n pad ... pad </s> v_1 ... v_m pad ... pad
 
-            video_len = video.shape[0]
-            video_tensor[i, :video_len] = video
-            video_attention_mask[i, :video_len] = True
+    #         video_len = video.shape[0]
+    #         video_tensor[i, :video_len] = video
+    #         video_attention_mask[i, :video_len] = True
 
-        attention_mask = torch.cat([text_attention_mask, video_attention_mask], 1)
+    #     attention_mask = torch.cat([text_attention_mask, video_attention_mask], 1)
 
-        return {"masked_caption_ids": text_tensor, "visual": video_tensor, "label_ids": labels,
-                "masked_caption": masked_captions,
-                "label": label_list, 'masked_caption_attention_mask': attention_mask}
+    #     return {"masked_caption_ids": text_tensor, "visual": video_tensor, "label_ids": labels,
+    #             "masked_caption": masked_captions,
+    #             "label": label_list, 'masked_caption_attention_mask': attention_mask}
 
 
 class QGenDataModule(pl.LightningDataModule):
@@ -145,12 +172,12 @@ class QGenDataModule(pl.LightningDataModule):
     def _dataloader(self, data_path: str, visual_data_path:str, batch_size: int, train: bool) -> DataLoader:
         dataset = QGenDataset(data_path, visual_data_path, tokenizer=self.tokenizer, return_visual=self.hasVisual)
         # TODO: bucket-batching could make training faster, and consume less memory.
-        if self.hasVisual:
-            return DataLoader(dataset, shuffle=train, batch_size=batch_size, num_workers=self.num_workers,
-                              pin_memory=True, collate_fn=dataset.collate_fn_multi_modal)
-        else:
-            return DataLoader(dataset, shuffle=train, batch_size=batch_size, num_workers=self.num_workers,
-                              pin_memory=True, collate_fn=dataset.collate_fn)
+        # if self.hasVisual:
+        #     return DataLoader(dataset, shuffle=train, batch_size=batch_size, num_workers=self.num_workers,
+        #                       pin_memory=True, collate_fn=dataset.collate_fn_multi_modal)
+        # else:
+        return DataLoader(dataset, shuffle=train, batch_size=batch_size, num_workers=self.num_workers,
+                            pin_memory=True, collate_fn=dataset.collate_fn)
 
     @overrides
     def train_dataloader(self, data_path: str = URL_DATA_TRAIN, visual_data_path: Optional[str] = None) -> DataLoader:
