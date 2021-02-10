e9d93fd91831811a2d5dbaf1327c3a716b12abdb
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2021-02-10 17:59:53 -0500

    Update textonly script.

--- scripts/great_lakes/submit_job_textonly.sh
+++ scripts/great_lakes/submit_job_textonly.sh
@@ -1,52 +1,49 @@
 #!/usr/bin/env bash
 
-#“#SBATCH” directives that convey submission options:
-
 #SBATCH --job-name=textonly
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
-#SBATCH --time=8:00:00
+#SBATCH --time=12:00:00
 #SBATCH --account=mihalcea1
 #SBATCH --partition=gpu
-# The application(s) to execute along with its input arguments and options:
+
 echo Started!
-# Use your own conda because Great Lakes ones are old and thus problematic.
-# source ~/.bashrc
 echo Hooking
 eval "$(conda shell.bash hook)"
 echo Sourcing
 conda activate lqam
 echo Sourced
 
-python -m scripts.evaluate_text_only_baseline --gpus=1 \
+echo evaluting greedy search
+echo evaluating "python -m scripts.run_model --gpus=1 --max-length=10 \
+--generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4 --batch-size=512"
+python -m scripts.run_model --gpus=1 \
 --generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4 --batch-size=512
-echo evaluating "python -m scripts.evaluate_text_only_baseline --batch-size=512 --gpus=1 --max-length=10 --generation-early-stopping --no-repeat-ngram-size=2 --num-workers=4"
 
-command="python -m scripts.evaluate_text_only_baseline --gpus=1 --max-length=10 --num-workers=4"
-
+echo evaluting beam search
+command="python -m scripts.run_model --gpus=1 --max-length=10 --num-workers=4"
 for beam_size in 2 4 8
 do
   for only_noun_phrase in 0 1
   do
-    batch_size=$((512 / beam_size))
-    new_command="${command} --beam-size=${beam_size} --batch-size=${batch_size}"
-    if [[ "$only_noun_phrase" == 1 ]]; then
-      new_command="${new_command} --only-noun-phrases"
-    fi
     for early_stopping in 0 1
     do
-      if [[ "$early_stopping" == 1 ]]; then
-        new_command="${new_command} --generation-early-stopping"
-      fi
       for no_repeat_n_gram in 0 1
       do
-        if [[ "$no_repeat_n_gram" == 1 ]]; then
-          new_command="${new_command} --no-repeat-ngram-size=2"
+        curr_command="${command} --beam-size=${beam_size} --batch-size=$((512 / beam_size))"
+        if [[ "$only_noun_phrase" == 1 ]]; then
+          curr_command+=" --only-noun-phrases"
         fi
-        echo "evaluating $new_command"
-        eval "$new_command"
+        if [[ "$early_stopping" == 1 ]]; then
+          curr_command+=" --generation-early-stopping"
+        fi
+        if [[ "$no_repeat_n_gram" == 1 ]]; then
+          curr_command+=" --no-repeat-ngram-size=2"
+        fi
+        echo "evaluating $curr_command"
+        eval "$curr_command"
       done
     done
   done
