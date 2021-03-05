1b243dda945e3c1483e5158edb8f004837645238
Ruoyao Wang <wsxzwps43@126.com>
2021-03-05 17:12:33 -0500

    add training script for the two stream model

---
+++ scripts/great_lakes/train_two_stream.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=two_stream
+#SBATCH --account=mihalcea1
+#SBATCH --partition=gpu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=50G
+#SBATCH --time=5:00:00
+
+source scripts/great_lakes/init.source
+PYTHONPATH=. python -u scripts/run_model.py --use-visual --two-stream --train --gpus 1 --num-workers 3 --batch-size 64
