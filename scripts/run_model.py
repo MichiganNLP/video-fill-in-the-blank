52c26fd39ba64e29bf6357ac6793f144071b2959
Nan Liu <45443761+nanlliu@users.noreply.github.com>
2021-03-12 19:55:19 -0500

    Combine all arguments into filler_kwargs.

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -94,16 +94,19 @@ def main() -> None:
                        "early_stopping": args.generation_early_stopping,
                        "no_repeat_ngram_size": args.no_repeat_ngram_size}
 
+    filler_kwargs = {
+        "t5_like_pretrained_model": t5_like_pretrained_model,
+        "tokenizer": tokenizer,
+        "only_noun_phrases": args.only_noun_phrases,
+        "lr": args.lr,
+        "lr_scheduler": args.lr_scheduler,
+        "weight_decay": args.weight_decay,
+        "generate_kwargs": generate_kwargs}
+
     if args.checkpoint_path:
-        filler = T5FillerModel.load_from_checkpoint(t5_like_pretrained_model=t5_like_pretrained_model,
-                                                    tokenizer=tokenizer,
-                                                    checkpoint_path=args.checkpoint_path,
-                                                    generate_kwargs=generate_kwargs)
+        filler = T5FillerModel.load_from_checkpoint(checkpoint_path=args.checkpoint_path, **filler_kwargs)
     else:
-        filler = T5FillerModel(t5_like_pretrained_model=t5_like_pretrained_model, tokenizer=tokenizer,
-                               only_noun_phrases=args.only_noun_phrases, lr=args.lr, lr_scheduler=args.lr_scheduler,
-                               weight_decay=args.weight_decay,
-                               generate_kwargs=generate_kwargs)
+        filler = T5FillerModel(**filler_kwargs)
 
     trainer = pl.Trainer(gpus=args.gpus, default_root_dir=args.trainer_default_root_dir, fast_dev_run=args.fast_dev_run,
                          max_epochs=args.epochs, benchmark=args.benchmark, deterministic=args.deterministic,
